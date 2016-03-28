f29934cfd886aecce8d04984a7f36e81d274265e
caklimas <caklimas@gmail.com>
2016-03-28 12:53:18 -0400

    Started working on Regex Language Service

---
+++ RegexLanguageService/RegexLanguageService/RegexScanner.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexLanguageService
+{
+    internal class RegexScanner : IScanner
+    {
+        private IVsTextBuffer buffer;
+        private string source;
+
+        public RegexScanner(IVsTextBuffer buffer)
+        {
+            this.buffer = buffer;
+        }
+
+        public bool ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
+        {
+            tokenInfo.Type = TokenType.Unknown;
+            tokenInfo.Color = TokenColor.Text;
+            return true;
+        }
+
+        public void SetSource(string source, int offset)
+        {
+            this.source = source.Substring(offset);
+        }
+    }
+}
